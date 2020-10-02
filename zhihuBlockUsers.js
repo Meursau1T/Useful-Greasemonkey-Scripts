b00865b2a4057029b49ed04ddcb86a86d490c724
MeursaulT <Wongxf@outlook.com>
2020-10-02 08:53:56 +0800

    Create zhihuBlockUsers.js
    在首页与回答中屏蔽特定知乎用户

---
+++ zhihuBlockUsers.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         知乎屏蔽用户
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       MeursaulT
+// @match        https://www.zhihu.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // 将想要屏蔽的用户写入userList即可
+    var userList = ['立党','苏莉安']
+    var answers = document.getElementsByClassName('ContentItem AnswerItem')
+    function deleteAns(){
+        for(var i = 0 ; i < answers.length ; i++){
+            for(var user of userList){
+                if(answers[i].dataset.zop.includes(user)){
+                    answers[i].remove()
+                }
+            }
+        }
+    }
+    (function(open) {
+        XMLHttpRequest.prototype.open = function() {
+            this.addEventListener("readystatechange", function() {
+                deleteAns();
+                console.log('done');
+            }, false);
+            open.apply(this, arguments);
+        };
+    })(XMLHttpRequest.prototype.open);
+
+})();
